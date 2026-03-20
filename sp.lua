b56f9ed97642dc9abd12e8525756ff99655da978
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-03-20 14:11:08 +0700

    Remove unnecessary newline in sp.lua

---
+++ sp.lua
@@ -0,0 +1,26 @@
+getgenv().Solix = {
+    ["Auto Farm"] = true,
+    ["Stat Allocation"] = "Auto (weapon-based)",
+    Reroll = {
+        ["Only Roll If have Title Lucky"] = { 
+            ["Enable"] = true, 
+            ["Title"] = "Destiny Marked" },
+        Clan = { 
+            ["Enable"] = true, 
+            ["Keep"] = {"Monarch"} },
+        Race = { 
+            ["Enable"] = true, 
+            ["Keep"] = {"Oni","Kitsune","Leviathan","Slime","Servant","Sunborn","Galevorn","Swordblessed"} },
+        Trait = { 
+            ["Enable"] = true, 
+            ["Keep"] = {"Overload","Cataclysm","Singularity","Celestial","Godspeed","Sovereign","Infinity","Malevolent"} },
+    },
+    ["Task Do After Max Level"] = { 
+        ["Auto Kill Saber Until Full Title"] = true, 
+        ["Farm Drops"] = true },
+    Performance = { 
+        ["FPS Lock"] = 30, 
+        ["Low CPU"] = false }
+}
+script_key="HyFmbhWguiBnAQvGXvAecQZwBxsVfCQo";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/meobeo8/a/a/a"))()
